09a1ea5646c6b99d79d9cdfe0446d307c58a65c8
sangu272 <sangeeta271713@gmail.com>
2024-11-02 11:05:11 +0530

    𝐓𝐆 𝐍𝐀𝐌𝐄

--- plugins/shayri.py
+++ plugins/shayri.py
@@ -86,10 +86,10 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/about_me_ankit"
+                        "✨𝚂𝚄𝙿𝙿𝙾𝚁𝚃✨", url=f"https://t.me/TG_BIO_STYLE/184"
                     ),
                     InlineKeyboardButton(
-                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/about_me_ankit"
+                        "✨𝙾𝙵𝙵𝙸𝙲𝙴✨", url=f"https://t.me/TG_BIO_STYLE/184"
                     ),
                 ]
             ]
